78f386b18bb33c4f4ea80bcfc2e47fd734f18cc4
Julian Kruithof <julian.kruithof@hva.nl>
2023-10-15 13:54:50 +0200

    Updated traffictracker tests

--- Assignment2/A2_TrafficControl_starter/src/test/java/models/TrafficTrackerTest2.java
+++ Assignment2/A2_TrafficControl_starter/src/test/java/models/TrafficTrackerTest2.java
@@ -3,15 +3,33 @@ package models;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TrafficTrackerTest2 {
     private final static String VAULT_NAME = "/2023-09";
 
     TrafficTracker trafficTracker;
+    Car volvo1, daf1;
+    Violation v1, v2, v3, v4;
 
     @BeforeEach
     public void setup() {
+        volvo1 = new Car("1-TTT-01", 5, Car.CarType.Truck, Car.FuelType.Diesel, LocalDate.of(2009,1,31));
+        daf1 = new Car("1-CCC-01", 5, Car.CarType.Coach, Car.FuelType.Diesel, LocalDate.of(2009,1,31));
+
+
+        //violations
+        v1 = new Violation(volvo1, "Amsterdam");
+        v1.setOffencesCount(5);
+        v2 = new Violation(volvo1, "Rotterdam");
+        v2.setOffencesCount(2);
+        v3 = new Violation(daf1, "Amsterdam");
+        v3.setOffencesCount(4);
+        v4 = new Violation(daf1, "Rotterdam");
+        v4.setOffencesCount(9);
 
         trafficTracker = new TrafficTracker();
 
@@ -26,6 +44,18 @@ public class TrafficTrackerTest2 {
         // When violations are empty.
         TrafficTracker emptyTrafficTracker = new TrafficTracker();
         assertEquals(0, emptyTrafficTracker.topViolationsByCar(5).size());
+
+        //adding violations to empty list
+        emptyTrafficTracker.getViolations().add(v1);
+        emptyTrafficTracker.getViolations().add(v2);
+        emptyTrafficTracker.getViolations().add(v3);
+        emptyTrafficTracker.getViolations().add(v4);
+
+        //get topViolationList
+        List<Violation> top = emptyTrafficTracker.topViolationsByCar(2);
+
+        assertEquals("1-CCC-01/null/13", top.get(0).toString());
+        assertEquals("1-TTT-01/null/7", top.get(1).toString());
 
         // When top violations are greater than violations size.
         assertThrows(IndexOutOfBoundsException.class, () -> trafficTracker.topViolationsByCar(100));
@@ -43,6 +73,20 @@ public class TrafficTrackerTest2 {
         TrafficTracker emptyTrafficTracker = new TrafficTracker();
         assertEquals(0, emptyTrafficTracker.topViolationsByCity(5).size());
 
+        //adding violations to empty list
+        emptyTrafficTracker.getViolations().add(v1);
+        emptyTrafficTracker.getViolations().add(v2);
+        emptyTrafficTracker.getViolations().add(v3);
+        emptyTrafficTracker.getViolations().add(v4);
+
+        //get topViolationList
+        List<Violation> top = emptyTrafficTracker.topViolationsByCity(2);
+
+        assertEquals("null/Rotterdam/11", top.get(0).toString());
+        assertEquals("null/Amsterdam/9", top.get(1).toString());
+
+
+
         // When top violations are greater than violations size.
         assertThrows(IndexOutOfBoundsException.class, () -> trafficTracker.topViolationsByCity(100));
 
@@ -56,8 +100,17 @@ public class TrafficTrackerTest2 {
     @Test
     public void finesAggregateCorrectly() {
         TrafficTracker emptyTrafficTracker = new TrafficTracker();
+
         //empty trafficTracker should have no violation to fine.
         assertEquals(0,  emptyTrafficTracker.calculateTotalFines());
+        //adding violations
+
+        emptyTrafficTracker.getViolations().add(v1);
+        emptyTrafficTracker.getViolations().add(v2);
+        emptyTrafficTracker.getViolations().add(v3);
+        emptyTrafficTracker.getViolations().add(v4);
+
+        assertEquals(630, emptyTrafficTracker.calculateTotalFines());
         assertEquals(186090, trafficTracker.calculateTotalFines());
     }
 }
