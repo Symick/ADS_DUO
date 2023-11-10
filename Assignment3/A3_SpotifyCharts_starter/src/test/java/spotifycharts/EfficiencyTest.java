ddb3c4906d61e202d45c15a7547bacdb941bb58d
Julian Kruithof <julian.kruithof@hva.nl>
2023-11-10 17:41:15 +0100

    Made an efficiency test

---
+++ Assignment3/A3_SpotifyCharts_starter/src/test/java/spotifycharts/EfficiencyTest.java
@@ -0,0 +1,95 @@
+package spotifycharts;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.EfficiencyTable;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class EfficiencyTest {
+    private final int ITERATIONS = 10;
+    private final int MAX_TIME = 20_000; // 20 seconds in milliseconds
+    private final int BIGGEST_INPUT_SIZE = 5_000_000;
+    private final double NANO_TO_MILLI = 1E-6;
+
+    long started;
+
+    private SongSorter songSorter;
+    private Comparator<Song> rankingScheme = Song::compareByHighestStreamsCountTotal;
+    //set up table
+    private EfficiencyTable<Double> results = new EfficiencyTable<>(4, 23, Double::sum, (a, b) -> a * b);
+
+    private List<Song> toBeSorted;
+
+    @BeforeEach
+    void setup() {
+        this.songSorter = new SongSorter();
+    }
+
+    @Test
+    void efficiencyTest() {
+        for (int i = 1; i <= ITERATIONS; i++) {
+            System.out.println("this is iteration -" + i);
+            //set up list
+            ChartsCalculator charts = new ChartsCalculator(i);
+
+            //keeps track of last duration, so after it takes longer than 20 seconds you can stop executing
+            double lastBubSelIntDuration = 0;
+            double lastQuickDuration = 0;
+            double lastTopHeapDuration = 0;
+
+
+            //loop over all input sizes and track on which row you are.
+            for (int n = 100, row = 0; n < BIGGEST_INPUT_SIZE; n *= 2, row++) {
+
+                //break out of iteration if all sorting algoritmes take longer than 20 seconds
+                if (lastQuickDuration >= MAX_TIME && lastBubSelIntDuration >= MAX_TIME && lastTopHeapDuration >= MAX_TIME) break;
+
+
+                //setup list with n size
+                List<Song> songs = charts.registerStreamedSongs(n);
+                //add the n column
+                results.add(0, row, (double) n);
+
+                //copy list
+                toBeSorted = new ArrayList<>(songs);
+                //bubble insertion or selection sort
+                System.gc();
+                if (lastBubSelIntDuration <= MAX_TIME) {
+                    started = System.nanoTime();
+                    songSorter.selInsBubSort(toBeSorted, rankingScheme);
+                    lastBubSelIntDuration = NANO_TO_MILLI * (System.nanoTime() - started);
+                    results.add(1, row, lastBubSelIntDuration);
+                }
+
+                //quicksort
+                toBeSorted = new ArrayList<>(songs);
+                System.gc();
+                if (lastQuickDuration <= MAX_TIME) {
+                    started = System.nanoTime();
+                    songSorter.quickSort(toBeSorted, rankingScheme);
+                    lastQuickDuration = NANO_TO_MILLI * (System.nanoTime() - started);
+                    results.add(2, row, lastQuickDuration);
+                }
+
+                //heapsort
+                toBeSorted = new ArrayList<>(songs);
+                System.gc();
+                if (lastTopHeapDuration <= MAX_TIME) {
+                    started = System.nanoTime();
+                    songSorter.topsHeapSort(100, toBeSorted, rankingScheme);
+                    lastTopHeapDuration = NANO_TO_MILLI * (System.nanoTime() - started);
+                    results.add(3, row, lastTopHeapDuration);
+                }
+            }
+        }
+
+        results.multiplyAll(1.0/ITERATIONS);
+
+        System.out.printf("\nSummary of %d repeats:\n", ITERATIONS);
+        System.out.println("N; T_selInsBub; T_quickSort; T_topsHeapSort");
+        System.out.println(results.csv("%.2f"));
+    }
+}
